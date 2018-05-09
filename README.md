6b8aa9036ba42d67a9654497aecf8542a8cd9a85
MscottB <skot_varg@hotmail.com>
2018-05-08 22:36:07 -0300

    new proyect

---
+++ README.md
@@ -0,0 +1,5 @@
+# Angular Starter Kit
+
+Ejemplo basico de una SPA, desarrollado en angular con temica de superHeroes.
+
+## [Ver Demo]()
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { AboutComponent } from './components/about/about.component';
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent, HomeComponent, HeroesComponent, HeroeComponent, BuscadorComponent, AboutComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
